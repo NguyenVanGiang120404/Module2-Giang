67e19f98e4b89f61ae7723ea7b4edcbb23ff0f78
giangFE <giangnvph37638@gmail.com>
2025-03-24 21:21:13 +0700

    table

--- Module2/lesson6/scrip.js
+++ Module2/lesson6/scrip.js
@@ -1,1 +1,9 @@
-alert('Xin Chào mọi người')
+// script.js
+document.getElementById('myButton').addEventListener('click', function() {
+    var message = "Hello, world!";
+    console.log(message);
+ });
+ function calculateSum(a, b) {
+    return a + b;
+ }
+ calculateSum(5, "10");
